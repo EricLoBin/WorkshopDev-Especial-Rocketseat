692bcf4e6956c390b6578365fb79da45ef7b74dc
EricLoBin <ekilobin@gmail.com>
2020-03-31 13:48:30 -0300

    [UPDATE] Aula 4

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
+const port = 3000
+
+//express para criar o servidor
 const express = require("express")
 const server = express()
 
-server.listen(3000)
+
+//rota criada
+server.get("/", function() {
+    console.log("vai")
+})
+
+
+//ligar servidor na porta 3000
+console.log("server open in port", port)
+server.listen(port)
