8e6c444d3e95eb805d0745cdb455e7f8213fe741
EricLoBin <ekilobin@gmail.com>
2020-04-01 16:09:09 -0300

    [UPDATE] Aula 5

--- db.js
+++ db.js
@@ -1,2 +1,32 @@
 const sqlite3 = require("sqlite3").verbose()
 const db = new sqlite3.Database('./ws.db')
+
+
+db.serialize(function() {
+
+    //criar tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT
+        );
+
+    `)
+    //inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES(?, ?, ?, ?, ?);
+    `
+    db.run(query, [
+        
+    ])
+})
